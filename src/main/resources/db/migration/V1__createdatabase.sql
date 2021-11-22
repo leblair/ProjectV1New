887bbaa96ab30ec2d1dc65d6cce243a41368ad1f
leblair <bryan.a.a7@gmail.com>
2021-11-22 18:42:56 +0100

    prueba run 3

--- src/main/resources/db/migration/V1__createdatabase.sql
+++ src/main/resources/db/migration/V1__createdatabase.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS anime (
     animeid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    text text,
     description text,
-    year int,
     type text,
+    year int,
     image text);
 
 
 INSERT INTO anime(description, year, type, image) VALUES
-    ('anime1',2020, 'type1','anime1.jpg'),
-    ('anime2', 2021,'type2','anime2.jpg'),
-    ('anime3',2021,'type3','anime3.jpg'),
-    ('anime4',2020,'type4','anime4.jpg');
+    ('text1','anime1',2020, 'type1','anime1.jpg'),
+    ('text2','anime2', 2021,'type2','anime2.jpg'),
+    ('text3','anime3',2021,'type3','anime3.jpg'),
+    ('text4','anime4',2020,'type4','anime4.jpg');
