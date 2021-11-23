8b6f88cebe1300d0c015a461e738315913495c7e
leblair <bryan.a.a7@gmail.com>
2021-11-23 19:30:49 +0100

    prueba run 4

--- build/resources/main/db/migration/V1__createdatabase.sql
+++ build/resources/main/db/migration/V1__createdatabase.sql
@@ -1,5 +1,14 @@
-CREATE TABLE IF NOT EXISTS movie (
-    movieid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    title text,
-    synopsis text,
-    imageurl text);
+CREATE TABLE IF NOT EXISTS anime (
+    animeid uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    text text,
+    description text,
+    type text,
+    year int,
+    image text);
+
+
+INSERT INTO anime(text,description, type, year, image) VALUES
+    ('text1','anime1','type1', 2020,'anime1.jpg'),
+    ('text2','anime2', 'type2',2021,'anime2.jpg'),
+    ('text3','anime3','type3',2019,'anime3.jpg'),
+    ('text4','anime4','type4',2018,'anime4.jpg');
